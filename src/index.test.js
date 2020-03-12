a4a58ff3d030821b1a232b63b0622cb4d539c19f
dayhaysoos <ndejesus1227@gmail.com>
2020-03-12 13:03:07 -0400

    cleaup not working

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act, cleanup } from '@testing-library/react-hooks';
 import { useStripeCart, CartProvider } from './index';
 
 // mock timer using jest
@@ -19,21 +19,58 @@ const INITIAL_STATE = {
   cancelUrl: 'https://egghead.io',
 };
 
-describe('useStripeCart', () => {
+const mockSku = {
+  sku: 'sku_abc123',
+  price: 200,
+  image: '',
+  currency: 'usd',
+};
+
+const createWrapper = () => ({ children }) => {
+  return (
+    <CartProvider
+      billingAddressCollection={false}
+      successUrl={'https://egghead.io'}
+      cancelUrl={'https://egghead.io'}
+      stripe={stripeMock}
+    >
+      {children}
+    </CartProvider>
+  );
+};
+
+describe('useStripeCart', async () => {
+  beforeEach(async () => await cleanup());
   it('renderps', () => {
-    const wrapper = ({ children }) => (
-      <CartProvider
-        billingAddressCollection={false}
-        successUrl={'https://egghead.io'}
-        cancelUrl={'https://egghead.io'}
-        stripe={stripeMock}
-      >
-        {children}
-      </CartProvider>
-    );
+    const wrapper = createWrapper();
     const { result } = renderHook(() => useStripeCart(), { wrapper });
 
     expect(result.current.cartItems).toEqual(INITIAL_STATE.cartItems);
+  });
+
+  it('addItems adds items to cart', () => {
+    const wrapper = createWrapper();
+    const { result } = renderHook(() => useStripeCart(), { wrapper });
+
+    act(() => {
+      result.current.addItem(mockSku);
+    });
+
+    expect(result.current.cartItems).toEqual([mockSku]);
+    cleanup();
+  });
+
+  it('cartCount increments when addItem is executed', () => {
+    const wrapper = createWrapper();
+    const { result } = renderHook(() => useStripeCart(), { wrapper });
+
+    expect(result.current.cartCount).toBe(0);
+
+    act(() => {
+      result.current.addItem(mockSku);
+    });
+
+    expect(result.current.cartCount).toBe(1);
   });
 });
 
