92e4658d8d86dd5f477f3f3c262b2d58ae582e40
ijones16 <jones58ian@gmail.com>
2020-03-13 13:56:34 -0400

    add tests for totalPrice

--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { renderHook, act, cleanup } from '@testing-library/react-hooks';
-import { useStripeCart, CartProvider } from './index';
+import React from 'react'
+import {renderHook, act} from '@testing-library/react-hooks'
+import {useStripeCart, CartProvider} from './index'
 
-afterEach(() => window.localStorage.clear());
+afterEach(() => window.localStorage.clear())
 
 const stripeMock = {
   redirectToCheckout: jest.fn().mockReturnValue(() => Promise.resolve()),
-};
+}
 
 const INITIAL_STATE = {
   lastClicked: '',
@@ -16,16 +16,23 @@ const INITIAL_STATE = {
   billingAddressCollection: false,
   successUrl: 'https://egghead.io',
   cancelUrl: 'https://egghead.io',
-};
+}
 
 const mockSku = {
   sku: 'sku_abc123',
   price: 200,
   image: '',
   currency: 'usd',
-};
+}
 
-const createWrapper = () => ({ children }) => {
+const mockSku3 = {
+  sku: 'sku_abc234',
+  price: 100,
+  image: '',
+  currency: 'usd',
+}
+
+const createWrapper = () => ({children}) => {
   return (
     <CartProvider
       billingAddressCollection={false}
@@ -35,87 +42,110 @@ const createWrapper = () => ({ children }) => {
     >
       {children}
     </CartProvider>
-  );
-};
+  )
+}
 
 describe('useStripeCart', () => {
   it('renderps', () => {
-    const wrapper = createWrapper();
-    const { result } = renderHook(() => useStripeCart(), { wrapper });
+    const wrapper = createWrapper()
+    const {result} = renderHook(() => useStripeCart(), {wrapper})
 
-    expect(result.current.cartItems).toEqual(INITIAL_STATE.cartItems);
-  });
+    expect(result.current.cartItems).toEqual(INITIAL_STATE.cartItems)
+  })
 
   it('addItems adds items to cart', () => {
-    const wrapper = createWrapper();
-    const { result } = renderHook(() => useStripeCart(), { wrapper });
+    const wrapper = createWrapper()
+    const {result} = renderHook(() => useStripeCart(), {wrapper})
 
     act(() => {
-      result.current.addItem(mockSku);
-    });
+      result.current.addItem(mockSku)
+    })
 
-    expect(result.current.cartItems).toEqual([mockSku]);
-  });
+    expect(result.current.cartItems).toEqual([mockSku])
+  })
 
   it('cartCount increments when addItem is executed', () => {
-    const wrapper = createWrapper();
-    const { result } = renderHook(() => useStripeCart(), { wrapper });
+    const wrapper = createWrapper()
+    const {result} = renderHook(() => useStripeCart(), {wrapper})
 
-    expect(result.current.cartCount).toBe(0);
+    expect(result.current.cartCount).toBe(0)
 
     act(() => {
-      result.current.addItem(mockSku);
-    });
+      result.current.addItem(mockSku)
+    })
 
-    expect(result.current.cartCount).toBe(1);
-  });
+    expect(result.current.cartCount).toBe(1)
+  })
 
   it('skus object updates with sku id and quantity based on addItems', () => {
-    const wrapper = createWrapper();
-    const { result } = renderHook(() => useStripeCart(), { wrapper });
+    const wrapper = createWrapper()
+    const {result} = renderHook(() => useStripeCart(), {wrapper})
 
-    expect(result.current.skus).toEqual({});
+    expect(result.current.skus).toEqual({})
 
     act(() => {
-      result.current.addItem(mockSku);
-    });
+      result.current.addItem(mockSku)
+    })
 
     expect(result.current.skus).toEqual({
       [mockSku.sku]: 1,
-    });
-  });
+    })
+  })
 
   it('checkoutData builds an array of objects to prepare for redirectToCheckout', () => {
-    const wrapper = createWrapper();
-    const { result } = renderHook(() => useStripeCart(), { wrapper });
+    const wrapper = createWrapper()
+    const {result} = renderHook(() => useStripeCart(), {wrapper})
 
-    expect(result.current.checkoutData).toEqual([]);
+    expect(result.current.checkoutData).toEqual([])
 
     act(() => {
-      result.current.addItem(mockSku);
-    });
+      result.current.addItem(mockSku)
+    })
 
     expect(result.current.checkoutData).toEqual([
-      { sku: mockSku.sku, quantity: 1 },
-    ]);
-  });
+      {sku: mockSku.sku, quantity: 1},
+    ])
+  })
 
   it('deleteItem removes item from sku object', () => {
-    const wrapper = createWrapper();
-    const { result } = renderHook(() => useStripeCart(), { wrapper });
+    const wrapper = createWrapper()
+    const {result} = renderHook(() => useStripeCart(), {wrapper})
 
     act(() => {
-      result.current.addItem(mockSku);
-    });
+      result.current.addItem(mockSku)
+    })
 
     expect(result.current.skus).toEqual({
       [mockSku.sku]: 1,
-    });
+    })
 
     act(() => {
-      result.current.deleteItem(mockSku.sku);
-    });
+      result.current.deleteItem(mockSku.sku)
+    })
 
-    expect(result.current.skus).toEqual({});
-  });
-});
+    expect(result.current.skus).toEqual({})
+  })
+
+  it('should update totalPrice', () => {
+    const wrapper = createWrapper()
+    const {result} = renderHook(() => useStripeCart(), {wrapper})
+
+    act(() => {
+      result.current.addItem(mockSku)
+    })
+
+    expect(result.current.totalPrice()).toBe('$2.00')
+  })
+
+  it('should update totalPrice when two items are added', () => {
+    const wrapper = createWrapper()
+    const {result} = renderHook(() => useStripeCart(), {wrapper})
+
+    act(() => {
+      result.current.addItem(mockSku)
+      result.current.addItem(mockSku3)
+    })
+
+    expect(result.current.totalPrice()).toBe('$3.00')
+  })
+})
