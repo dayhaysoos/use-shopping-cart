43f8bfdc424d123a33b9f9e6de7e4ee8465ff06f
dayhaysoos <ndejesus1227@gmail.com>
2020-04-04 17:00:02 -0400

    add language

--- src/index.js
+++ src/index.js
@@ -41,8 +41,8 @@ const formatDetailedCart = (currency, cartItems, language) => {
   }, {});
 };
 
-const formatCheckoutCart = checkoutData => {
-  return Object.keys(checkoutData).map(item => ({
+const formatCheckoutCart = (checkoutData) => {
+  return Object.keys(checkoutData).map((item) => ({
     sku: item,
     quantity: checkoutData[item],
   }));
@@ -58,7 +58,7 @@ const updateQuantity = (quantity, skuID, skus) => {
 };
 
 const removeItem = (skuID, cartItems) => {
-  const index = cartItems.findIndex(item => item.sku);
+  const index = cartItems.findIndex((item) => item.sku);
   return cartItems;
 };
 
@@ -99,7 +99,7 @@ const reducer = (cart, action) => {
           JSON.stringify(removeSku(action.skuID, skus))
         );
 
-      const index = cartItems.findIndex(item => item.sku === action.skuID);
+      const index = cartItems.findIndex((item) => item.sku === action.skuID);
 
       if (index !== -1) {
         cartItems.splice(index, 1);
@@ -158,7 +158,7 @@ export const CartProvider = ({
   successUrl,
   cancelUrl,
   currency,
-  language=navigator.language,
+  language = navigator.language,
 }) => {
   const skuStorage =
     typeof window !== 'undefined'
@@ -196,7 +196,7 @@ export const useStripeCart = () => {
     successUrl,
     cancelUrl,
     currency,
-    language
+    language,
   } = cart;
 
   let storageReference =
@@ -208,7 +208,7 @@ export const useStripeCart = () => {
   }
 
   const checkoutData = formatCheckoutCart(skus);
-  
+
   const totalPrice = () => calculateTotalPrice(currency, cartItems);
 
   typeof localStorage === 'object' &&
@@ -221,21 +221,21 @@ export const useStripeCart = () => {
     0
   );
 
-  const addItem = sku => {
+  const addItem = (sku) => {
     dispatch({ type: 'addToCheckoutCart', sku });
     dispatch({ type: 'addToCartItems', sku });
   };
 
-  const removeCartItem = sku => {
+  const removeCartItem = (sku) => {
     dispatch({ type: 'removeFromCartItems', sku });
   };
   const handleQuantityChange = (quantity, skuID) => {
     dispatch({ type: 'handleQuantityChange', quantity, skuID });
   };
 
-  const deleteItem = skuID => dispatch({ type: 'delete', skuID });
+  const deleteItem = (skuID) => dispatch({ type: 'delete', skuID });
 
-  const storeLastClicked = skuID =>
+  const storeLastClicked = (skuID) =>
     dispatch({ type: 'storeLastClicked', skuID });
 
   const handleCartClick = () => dispatch({ type: 'cartClick' });
