777e3e6264f438acf2396da1e18d3cd64dd715f5
Kevin Cunningham <kevin@kevincunningham.co.uk>
2020-04-05 16:53:20 +0100

    Refactors utils to refer to value rather than price.

--- src/util.js
+++ src/util.js
@@ -1,13 +1,13 @@
-export const toCurrency = ({ price, currency, language }) => {
+export const toCurrency = ({ value, currency, language }) => {
   const formatted = new Intl.NumberFormat(language, {
     style: 'currency',
     currency,
-  }).format(price / 100);
+  }).format(value / 100);
 
   return formatted;
 };
 
-export const calculateTotalPrice = (currency, cartItems) => {
-  const price = cartItems.reduce((acc, { price }) => acc + price, 0);
-  return toCurrency({ price, currency });
+export const calculateTotalValue = (currency, cartItems) => {
+  const value = cartItems.reduce((acc, { price }) => acc + price, 0);
+  return toCurrency({ value, currency });
 };
