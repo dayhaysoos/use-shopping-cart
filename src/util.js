60d682789db189b3ad9615fc12976f445ec98352
ChrisBrownie55 <chbphone55@gmail.com>
2020-04-11 09:44:00 -0500

    separate cartItems from cart reducer; integrate localstorage; simplified action types

--- src/util.js
+++ src/util.js
@@ -1,9 +1,12 @@
+import { useStorageReducer } from 'react-storage-hooks';
+
+export const isClient = typeof window === 'object';
+
 export const toCurrency = ({ value, currency, language }) => {
   const formatted = new Intl.NumberFormat(language, {
     style: 'currency',
     currency,
   }).format(value / 100);
-
   return formatted;
 };
 
@@ -11,3 +14,17 @@ export const calculateTotalValue = (currency, cartItems) => {
   const value = cartItems.reduce((acc, { price }) => acc + price, 0);
   return toCurrency({ value, currency });
 };
+
+export function useLocalStorageReducer(key, reducer, initialState) {
+  const dummyStorage = {
+    getItem() { return null; },
+    setItem() {},
+    removeItem() {},
+  };
+  return useStorageReducer(
+    isClient ? window.localStorage : dummyStorage,
+    key,
+    reducer,
+    initialState,
+  );
+}
