393bf87b787b8c3790efa39ecb76c50105408ae7
dayhaysoos <ndejesus1227@gmail.com>
2020-04-11 21:57:23 -0400

    add serverless function, created product json reference

---
+++ example/functions/create-session.js
@@ -0,0 +1,57 @@
+/*
+ * This function creates a Stripe Checkout session and returns the session ID
+ * for use with Stripe.js (specifically the redirectToCheckout method).
+ *
+ * @see https://stripe.com/docs/payments/checkout/one-time
+ */
+const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
+
+/*
+ * Product data can be loaded from anywhere. In this case, we’re loading it from
+ * a local JSON file, but this could also come from an async call to your
+ * inventory management service, a database query, or some other API call.
+ *
+ * The important thing is that the product info is loaded from somewhere trusted
+ * so you know the pricing information is accurate.
+ */
+const inventory = require('./data/products.json');
+
+exports.handler = async (event) => {
+  const { sku, quantity } = JSON.parse(event.body);
+  const product = inventory.find((p) => p.sku === sku);
+
+  // ensure that the quantity is within the allowed range
+  const validatedQuantity = quantity > 0 && quantity < 11 ? quantity : 1;
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    billing_address_collection: 'auto',
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    },
+
+    /*
+     * This env var is set by Netlify and inserts the live site URL. If you want
+     * to use a different URL, you can hard-code it here or check out the
+     * other environment variables Netlify exposes:
+     * https://docs.netlify.com/configure-builds/environment-variables/
+     */
+    success_url: `${process.env.URL}/success.html`,
+    cancel_url: process.env.URL,
+    line_items: [
+      {
+        name: product.name,
+        description: product.description,
+        images: [product.image],
+        amount: product.amount,
+        currency: product.currency,
+        quantity: validatedQuantity,
+      },
+    ],
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ sessionId: session.id }),
+  };
+};
