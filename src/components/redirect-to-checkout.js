565df9c5f370c03a05a3928591f1b3d0d8ef8c2c
dayhaysoos <ndejesus1227@gmail.com>
2020-04-30 14:54:41 -0400

    redirectToCheckout component and docs

---
+++ src/components/redirect-to-checkout.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { useShoppingCart, formatCurrencyString } from 'use-shopping-cart'
+
+export function RedirectToCheckout({ product }) {
+  const { redirectToCheckout, cartDetails } = useShoppingCart()
+
+  const hasItems = !Object.keys(cartDetails).length > 0
+
+  /* A helper function that turns the price into a readable format */
+  const price = formatCurrencyString({
+    value: product.price,
+    currency: product.currency,
+    language: 'en-US',
+  })
+  return (
+    <article
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '50%',
+      }}
+    >
+      <figure style={{ textAlign: 'center' }}>
+        <img
+          style={{ height: 200, width: 250 }}
+          src={product.image}
+          alt={` ${product.name}`}
+        />
+        <figcaption>{product.name}</figcaption>
+      </figure>
+      <p>{price}</p>
+
+      {hasItems && (
+        <p style={{ color: 'red' }}>
+          Please go to addItem() and add an item to the cart
+        </p>
+      )}
+
+      <button
+        disabled={hasItems}
+        onClick={() => redirectToCheckout()}
+        aria-label={`Add ${product.name} to your cart`}
+        style={{ height: 50, width: 100, marginBottom: 30 }}
+      >
+        Checkout
+      </button>
+    </article>
+  )
+}
+
+export default RedirectToCheckout
