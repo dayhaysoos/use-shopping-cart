8085648b01e81a563354925bc7111e26181f9d02
dayhaysoos <ndejesus1227@gmail.com>
2020-05-01 16:01:10 -0400

    handle language better

--- use-shopping-cart/src/util.js
+++ use-shopping-cart/src/util.js
@@ -2,16 +2,17 @@ import { useStorageReducer } from 'react-storage-hooks'
 
 export const isClient = typeof window === 'object'
 
-export const formatCurrencyString = ({ value, currency }) => {
+export const formatCurrencyString = ({
+  value,
+  currency,
+  language = isClient ? navigator.language : 'en-US'
+}) => {
   value = parseInt(value)
-  const numberFormat = new Intl.NumberFormat(
-    isClient ? window.navigator.language : 'en-US',
-    {
-      style: 'currency',
-      currency,
-      currencyDisplay: 'symbol'
-    }
-  )
+  const numberFormat = new Intl.NumberFormat(language, {
+    style: 'currency',
+    currency,
+    currencyDisplay: 'symbol'
+  })
   const parts = numberFormat.formatToParts(value)
   let zeroDecimalCurrency = true
   for (const part of parts) {
