5d6e9a44fa8fb1b883052fa2dd3cd27a6bdf6518
dayhaysoos <ndejesus1227@gmail.com>
2020-05-01 16:06:07 -0400

    import isClient

--- use-shopping-cart/src/index.js
+++ use-shopping-cart/src/index.js
@@ -8,7 +8,8 @@ import React, {
 import {
   formatCurrencyString,
   calculateTotalValue,
-  useLocalStorageReducer
+  useLocalStorageReducer,
+  isClient
 } from './util'
 import PropTypes from 'prop-types'
 
