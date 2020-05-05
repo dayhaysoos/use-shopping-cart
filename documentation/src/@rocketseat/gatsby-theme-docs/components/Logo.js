5cd9388c7b58bd9969bc86b16dc66de7102ae604
ChrisBrownie55 <chbphone55@gmail.com>
2020-05-05 14:54:06 -0500

    documentation

--- documentation/src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++ documentation/src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -4,18 +4,17 @@ require('typeface-bungee')
 
 const Logo = () => {
   return (
-    <Link
-      aria-label={'Use Shopping Cart'}
+    <span
+      aria-label="Use Shopping Cart"
       style={{
         backgroundColor: 'white',
         color: '#3E4C93',
         fontFamily: 'bungee',
       }}
-      to={'/'}
     >
       use-shopping-cart
-    </Link>
-  )
-}
+    </span>
+  );
+};
 
 export default Logo
