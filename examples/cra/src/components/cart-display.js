239e76a224e7ed1e3d7e324e8a14a5a1e2c064b0
Christopher Brown <chbphone55@gmail.com>
2020-05-07 16:39:48 -0500

    Refactor/no cart items (#98)
    * oop
    * working on it
    * working on making sure it works
    * more tests
    * addItem() is now tested
    * incrementItem() now tested
    * new tests passing!
    * yee
    * get formattedTotalPrice
    * yee
    * fixed tests
    * tests
    * tests
    * update cart display with increment/decrementItem
    * remove add and decrease buttons in favor of number input
    * change cart-items key to cart-values
    
    Co-authored-by: dayhaysoos <ndejesus1227@gmail.com>

--- examples/cra/src/components/cart-display.js
+++ examples/cra/src/components/cart-display.js
@@ -1,21 +1,20 @@
 /**@jsx jsx */
-import { jsx, Box, Flex, Image, Button } from 'theme-ui';
+import { jsx, Box, Flex, Image, Button, Input } from 'theme-ui';
 import { useShoppingCart } from 'use-shopping-cart';
 
 const CartDisplay = () => {
   const {
     cartDetails,
-    cartItems,
     cartCount,
-    addItem,
-    removeCartItem,
-    totalPrice,
+    decrementItem,
+    incrementItem,
+    formattedTotalPrice,
     redirectToCheckout,
-    reduceItemByOne,
     clearCart,
+    setItemQuantity,
   } = useShoppingCart();
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async event => {
     event.preventDefault();
 
     const response = await fetch('/.netlify/functions/create-session', {
@@ -25,10 +24,10 @@ const CartDisplay = () => {
       },
       body: JSON.stringify(cartDetails),
     })
-      .then((res) => {
+      .then(res => {
         return res.json();
       })
-      .catch((error) => console.log(error));
+      .catch(error => console.log(error));
 
     redirectToCheckout({ sessionId: response.sessionId });
   };
@@ -50,14 +49,14 @@ const CartDisplay = () => {
         }}
       >
         <h2>Shopping Cart Display Panel</h2>
-        {Object.keys(cartDetails).map((item) => {
+        {Object.keys(cartDetails).map(item => {
           const cartItem = cartDetails[item];
-          const { name, sku, formattedValue, quantity } = cartItem;
+          const { name, sku, quantity } = cartItem;
           return (
             <Flex
               key={cartItem.sku}
               sx={{
-                justifyContent: 'space-between',
+                justifyContent: 'space-around',
                 alignItems: 'center',
                 width: '100%',
               }}
@@ -66,27 +65,21 @@ const CartDisplay = () => {
                 <Image sx={{ width: 100 }} src={cartItem.image} />
                 <p>{name}</p>
               </Flex>
-              <Box>
-                <span sx={{ display: 'block' }}>Price: {formattedValue}</span>
-                <span sx={{ display: 'block' }}>qty: {quantity}</span>
-                <Button
-                  backgroundColor={'black'}
-                  onClick={() => addItem(cartItem)}
-                >
-                  +
-                </Button>
-                <Button
-                  backgroundColor={'black'}
-                  onClick={() => reduceItemByOne(sku)}
-                >
-                  -
-                </Button>
-              </Box>
+              <Input
+                type={'number'}
+                max={99}
+                sx={{ width: 60 }}
+                defaultValue={quantity}
+                onChange={e => {
+                  const { value } = e.target;
+                  setItemQuantity(sku, value);
+                }}
+              />
             </Flex>
           );
         })}
         <h3>Total Items in Cart: {cartCount}</h3>
-        <h3>Total Price: {totalPrice()}</h3>
+        <h3>Total Price: {formattedTotalPrice}</h3>
         <Box
           as={'form'}
           action={'/.netlify/functions/create-session'}
--- use-shopping-cart/src/serverUtil.js
+++ use-shopping-cart/src/serverUtil.js
@@ -1,8 +1,6 @@
 const validateCartItems = (inventorySrc, cartDetails) => {
   const validatedItems = []
   for (const sku in cartDetails) {
-    if (!Object.prototype.hasOwnProperty.call(cartDetails, sku)) continue
-
     const product = cartDetails[sku]
     const inventoryItem = inventorySrc.find(
       currentProduct => currentProduct.sku === sku
