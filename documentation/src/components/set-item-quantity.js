50b10560f417296d43d034b82c41f19dc74c59da
ChrisBrownie55 <chbphone55@gmail.com>
2020-05-13 18:24:37 -0500

    update setItemQuantity example for Checkout Experience

--- documentation/src/components/set-item-quantity.js
+++ documentation/src/components/set-item-quantity.js
@@ -1,54 +1,58 @@
 import React from 'react'
-import { useShoppingCart, formatCurrencyString } from 'use-shopping-cart'
+import { useShoppingCart } from 'use-shopping-cart'
 
-export function SetItemQuantity({ product }) {
+export function SetItemQuantity() {
   const { setItemQuantity, cartDetails } = useShoppingCart()
 
-  const itemQuantity = !cartDetails[product.sku]
-    ? 0
-    : cartDetails[product.sku].quantity
+  const entries = []
+  for (const sku in cartDetails) {
+    const entry = cartDetails[sku]
 
-  /* A helper function that turns the price into a readable format */
-  const price = formatCurrencyString({
-    value: product.price,
-    currency: product.currency,
-    language: 'en-US'
-  })
-  return (
-    <article
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '50%'
-      }}
-    >
-      <figure style={{ textAlign: 'center' }}>
-        <img
-          style={{ height: 200, width: 250 }}
-          src={product.image}
-          alt={product.name}
-        />
-        <figcaption>{product.name}</figcaption>
-      </figure>
-      <p>{price}</p>
-      {/* Dropdown to change the item's quantity */}
-      <select
-        defaultValue={itemQuantity}
-        onChange={(event) => {
-          setItemQuantity(product.sku, event.target.value)
+    const options = []
+    for (let quantity = 1; quantity <= 20; ++quantity)
+      options.push(<option value={quantity}>{quantity}</option>)
+
+    entries.push(
+      <article
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '50%'
         }}
       >
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-      </select>
-    </article>
-  )
+        <figure style={{ textAlign: 'center' }}>
+          <img
+            style={{ height: 200, width: 250 }}
+            src={entry.image}
+            alt={entry.name}
+          />
+          <figcaption>{entry.name}</figcaption>
+        </figure>
+        <p>{entry.formattedValue}</p>
+
+        <div style={{ display: 'flex', alignItem: 'center' }}>
+          {/* Dropdown to change the item's quantity */}
+          <label htmlFor="quantity-select" style={{ margin: '0 1rem 0 0' }}>
+            Quantity:
+          </label>
+          <select
+            id="quantity-select"
+            defaultValue={entry.quantity}
+            onChange={(event) => {
+              setItemQuantity(sku, event.target.value)
+            }}
+          >
+            {options}
+          </select>
+        </div>
+      </article>
+    )
+  }
+
+  if (entries.length) return entries
+  return <p>You currently don't have any items in your cart.</p>
 }
 
 export default SetItemQuantity
