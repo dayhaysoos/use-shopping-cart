ce0937067f767586f6fc7456ee237607c0a2b3ba
dayhaysoos <ndejesus1227@gmail.com>
2020-05-17 19:35:51 -0400

    added checkout button (needs serious styling)

--- examples/cra/src/components/product.js
+++ examples/cra/src/components/product.js
@@ -3,8 +3,28 @@ import { jsx, Box, Image, Button, Flex } from 'theme-ui'
 import { useShoppingCart, formatCurrencyString } from 'use-shopping-cart'
 
 const Product = (product) => {
-  const { addItem } = useShoppingCart()
+  const { addItem, checkoutSingleItem } = useShoppingCart()
   const { name, price, image, currency } = product
+
+  const handleSubmit = async (product) => {
+    const response = await fetch(
+      '/.netlify/functions/one-click-create-session',
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ [product.sku]: { ...product, quantity: 1 } })
+      }
+    )
+      .then((res) => {
+        return res.json()
+      })
+      .catch((error) => console.log(error))
+
+    checkoutSingleItem({ sessionId: response.sessionId })
+  }
+
   return (
     <Flex
       sx={{
@@ -16,10 +36,13 @@ const Product = (product) => {
       <Image src={image} />
       <Box>
         <p>{name}</p>
-        <p>{formatCurrencyString({ price: price, currency })}</p>
+        <p>{formatCurrencyString({ value: price, currency })}</p>
       </Box>
       <Button onClick={() => addItem(product)} backgroundColor={'black'}>
         Add To Cart
+      </Button>
+      <Button onClick={() => handleSubmit(product)} backgroundColor={'black'}>
+        Checkout
       </Button>
     </Flex>
   )
