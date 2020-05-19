0face7f03b2a536d8830dc16cb41e99938006b93
ChrisBrownie55 <chbphone55@gmail.com>
2020-05-19 17:07:15 -0500

    This should fix README needing to be in two places at once

---
+++ scripts/clone-readme.cmd
@@ -0,0 +1,4 @@
+IF EXIST "README.md" (
+  del README.md
+)
+copy use-shopping-cart\README.md README.md
---
+++ scripts/clone-readme.ps1
@@ -0,0 +1,4 @@
+if (Test-Path -Path README.md) {
+  Remove-Item -Path README.md
+}
+Copy-Item -Path use-shopping-cart\README.md -Destination README.md
