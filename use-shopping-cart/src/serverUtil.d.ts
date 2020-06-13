595a5b30e6aea1cf19ff5a0b860d29f3b770e6f4
Thorsten Schaeff <thorsten@stripe.com>
2020-06-13 20:16:37 +0800

    Fix type declaration formatting.

--- use-shopping-cart/src/serverUtil.d.ts
+++ use-shopping-cart/src/serverUtil.d.ts
@@ -1,6 +1,9 @@
-import * as useShoppingCart from 'use-shopping-cart';
+import * as useShoppingCart from 'use-shopping-cart'
 import Stripe from 'stripe'
 
 declare module 'use-shopping-cart/src/serverUtil' {
-  export function validateCartItems(inventory: useShoppingCart.Product[], cartItems: useShoppingCart.CartDetails): Stripe.Checkout.SessionCreateParams.LineItem[];
+  export function validateCartItems(
+    inventory: useShoppingCart.Product[],
+    cartItems: useShoppingCart.CartDetails
+  ): Stripe.Checkout.SessionCreateParams.LineItem[]
 }
