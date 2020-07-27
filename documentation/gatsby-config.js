f22a390b8154fd29ed28df66367fe3707911897f
sballgirl11 <brittney.postma@hotmail.com>
2020-07-26 20:07:53 -0400

    changed config back

--- documentation/gatsby-config.js
+++ documentation/gatsby-config.js
@@ -34,31 +34,31 @@ module.exports = {
         icon: `static/favicon.png`,
         icons: [
           {
-            src: '/favicon-16x16.png',
-            sizes: '16x16',
-            type: 'image/png'
+            src: "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
           },
           {
-            src: '/favicon-32x32.png',
-            sizes: '32x32',
-            type: 'image/png'
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
           },
           {
-            src: '/logo.png',
-            sizes: '100x100',
-            type: 'image/png'
+            src: "/logo.png",
+            sizes: "100x100",
+            type: "image/png",
           },
           {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       }
     },
     `gatsby-plugin-sitemap`,
