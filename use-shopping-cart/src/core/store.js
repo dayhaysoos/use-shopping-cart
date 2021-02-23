a38de75a5b31eb70446df60adccb08c8c154a3f5
Nick DeJesus <ndejesus1227@gmail.com>
2021-02-23 00:47:03 -0500

    relocation

--- use-shopping-cart/src/core/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartReducer from '../core/slices/cartSlice'
-
-export default configureStore({
-  reducer: {
-    cart: cartReducer
-  }
-})
