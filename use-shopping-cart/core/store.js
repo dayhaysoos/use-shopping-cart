f61af9a221171c56140f8979e812a49dabc4400f
Nick DeJesus <ndejesus1227@gmail.com>
2021-02-28 00:38:22 -0500

    added formatCurrencyString to store

--- use-shopping-cart/core/store.js
+++ use-shopping-cart/core/store.js
@@ -1,6 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { reducer, actions, initialState } from './slice'
 
+export const formatCurrencyString = ({
+  value,
+  currency,
+  language = isClient ? navigator.language : 'en-US'
+}) => {
+  value = parseInt(value)
+  const numberFormat = new Intl.NumberFormat(language, {
+    style: 'currency',
+    currency,
+    currencyDisplay: 'symbol'
+  })
+  const parts = numberFormat.formatToParts(value)
+  let zeroDecimalCurrency = true
+
+  for (const part of parts) {
+    if (part.type === 'decimal') {
+      zeroDecimalCurrency = false
+      break
+    }
+  }
+
+  value = zeroDecimalCurrency ? value : value / 100
+  return numberFormat.format(value.toFixed(2))
+}
+
 export { reducer, actions }
 export function createShoppingCartStore(options) {
   return configureStore({
