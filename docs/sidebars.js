6128153d8e45f765413c9dbf0b582bd3cebf3a5f
ehowey <ehowey@gmail.com>
2021-03-01 21:50:00 -0700

    Basic working build

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,6 +1,23 @@
 module.exports = {
   docs: {
-    Welcome: ['introduction', 'getting-started', 'discord', 'contributors']
+    Welcome: ['introduction', 'getting-started', 'discord', 'contributors'],
+    Usage: [
+      'usage/add-item',
+      'usage/api',
+      'usage/cart-provider',
+      'usage/checkout-single-item',
+      'usage/clear-cart',
+      'usage/debug-cart',
+      'usage/decrement-item',
+      'usage/format-line-items',
+      'usage/increment-item',
+      'usage/load-cart',
+      'usage/redirect-to-checkout',
+      'usage/remove-cart-item',
+      'usage/set-item-quantity',
+      'usage/validate-cart-items'
+    ],
+    Util: ['util/format-currency-string']
     // Docusaurus: ['doc1', 'doc2', 'doc3'],
     // Features: ['mdx']
   }
