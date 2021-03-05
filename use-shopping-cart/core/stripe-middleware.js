abbb39f4bbd313890fa1579aa669188c5911f29f
Nick DeJesus <ndejesus1227@gmail.com>
2021-03-05 18:34:28 -0500

    created stripe middleware to manage getting to checkout

---
+++ use-shopping-cart/core/stripe-middleware.js
@@ -0,0 +1,31 @@
+import { loadStripe } from '@stripe/stripe-js'
+import { getCheckoutData } from '../utilities/old-utils'
+
+export const handleStripe = (store) => (next) => async (action) => {
+  if (action.type === 'cart/redirectToCheckout') {
+    try {
+      const stripePublicKey = store.getState().stripe
+
+      const result = await loadStripe(stripePublicKey)
+      const cart = store.getState()
+
+      if (action.payload?.sessionId) {
+        return result.redirectToCheckout({
+          sessionId: action.payload.sessionId
+        })
+      }
+
+      if (store.getState().mode === 'client-only') {
+        const checkoutData = getCheckoutData(cart)
+        result.redirectToCheckout(checkoutData)
+      }
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+  try {
+    return next(action)
+  } catch (error) {
+    console.error('Error:', err)
+  }
+}
