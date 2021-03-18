73f38322573188e3b2dda59475b58a768d448f23
Nick DeJesus <ndejesus1227@gmail.com>
2021-03-17 23:07:57 -0400

    add check for window for redux persist

--- use-shopping-cart/core/store.js
+++ use-shopping-cart/core/store.js
@@ -13,7 +13,24 @@ import {
   PURGE,
   REGISTER
 } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import persistStorage from 'redux-persist/lib/storage'
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null)
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value)
+    },
+    removeItem(_key) {
+      return Promise.resolve()
+    }
+  }
+}
+
+const storage =
+  typeof window === 'undefined' ? createNoopStorage() : persistStorage
 
 export { reducer, actions, filterCart, formatCurrencyString }
 export function createShoppingCartStore(options) {
