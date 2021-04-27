98fc004f69a37a39f57d587b83ea299007db7ec8
Nick DeJesus <ndejesus1227@gmail.com>
2021-04-27 10:27:20 -0400

    update formatLineItems to look for id

--- use-shopping-cart/utilities/serverless.js
+++ use-shopping-cart/utilities/serverless.js
@@ -38,7 +38,9 @@ function formatLineItems(cartDetails) {
   const lineItems = []
 
   for (const itemId in cartDetails) {
-    if (cartDetails[itemId].sku_id || cartDetails[itemId].price_id)
+    const item = cartDetails[itemId]
+
+    if (cartDetails[itemId].id)
       lineItems.push({ price: itemId, quantity: cartDetails[itemId].quantity })
   }
 
