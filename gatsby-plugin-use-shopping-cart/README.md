99ec786d5ea5212d84f6679637f1c1caa01ac7d5
ehowey <ehowey@gmail.com>
2021-05-30 22:56:35 -0600

    Add plugin

---
+++ gatsby-plugin-use-shopping-cart/README.md
@@ -0,0 +1,14 @@
+# Gatsby Plugin Use-Shopping-Cart
+
+Serious WIP.
+
+Here is the first draft of the API.
+
+mode - String
+cartMode - String
+stripePublicKey - String
+successUrl - String
+cancelUrl - String
+currency - String
+allowedCountries - Array of strings
+billingAddressCollection - Boolean
---
+++ gatsby-plugin-use-shopping-cart/gatsby-node.js
@@ -0,0 +1,45 @@
+//     Current API
+//     mode,
+//     stripePublicKey,
+//     successUrl,
+//     cancelUrl,
+//     currency,
+//     allowedCountries,
+//     billingAddressCollection
+
+exports.pluginOptionsSchema = ({ Joi }) => {
+  return Joi.object({
+    mode: Joi.string()
+      .default(`payment`)
+      .description(`Mode should be "payment"`),
+    cartMode: Joi.string()
+      .default(`client-only`)
+      .description(`Cart mode can be 'client-only' or 'checkout-session'.`),
+    stripePublicKey: Joi.string()
+      .required()
+      .description(`Public key from your Stripe dashboard.`)
+      .messages({
+        // Override the error message if the .required() call fails
+        "any.required": `"Stripe Public Key" needs to be provided. Get the correct value from your Stripe dashboard`,
+      }),
+    successUrl: Joi.string()
+      .default(`https://useshoppingcart.com/`)
+      .description(`Url redirect on a successful purchase.`),
+    cancelUrl: Joi.string()
+      .default(`https://stripe.com/`)
+      .description(`Url redirect on a cancelled purchase.`),
+    // allowedCountries: ["US", "GB", "CA"],
+    allowedCountries: Joi.array()
+      .items(Joi.string())
+      .default(["US", "GB", "CA"])
+      .description(
+        `Array of allowed countries for Stripe purchases, use international two letter country codes. `
+      ),
+    currency: Joi.string()
+      .default(`USD`)
+      .description(`Currency for transaction, see Stripe docs for details.`),
+    billingAddressCollection: Joi.boolean()
+      .default(true)
+      .description(`Boolean to enable billing address collection.`),
+  })
+}
