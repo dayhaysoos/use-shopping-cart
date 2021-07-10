5202c99712e65715e8437c99c9cd05bb7e5daeca
Nick DeJesus <ndejesus1227@gmail.com>
2021-07-10 00:16:40 -0400

    remove old docs site

--- documentation/gatsby-node.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      modules: [path.resolve(__dirname, 'src'), 'node_modules']
-    }
-  })
-}
