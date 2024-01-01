567d0bb6cc117c803b1d411248a56063f7dacca6
Nick DeJesus <ndejesus1227@gmail.com>
2023-12-31 19:05:15 -0500

    finally fix next js warning

--- use-shopping-cart/react/index.js
+++ use-shopping-cart/react/index.js
@@ -18,22 +18,26 @@ export const useSelector = createSelectorHook(CartContext)
 export const useDispatch = createDispatchHook(CartContext)
 
 export function CartProvider({ loading = null, children, ...props }) {
+  const [isClient, setIsClient] = React.useState(false)
+
+  React.useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   const store = React.useMemo(() => createShoppingCartStore(props), [props])
 
-  if (!props.shouldPersist) {
-    return (
-      <Provider context={CartContext} store={store}>
-        {children}
-      </Provider>
-    )
-  }
-
-  if (props.shouldPersist) {
+  if (props.shouldPersist && isClient) {
     const persistor = persistStore(store)
 
     return (
       <Provider context={CartContext} store={store}>
         <PersistGate persistor={persistor}>{() => children}</PersistGate>
+      </Provider>
+    )
+  } else {
+    return (
+      <Provider context={CartContext} store={store}>
+        {children}
       </Provider>
     )
   }
