b9a23c64a50c326cc51647e5ccc04104d1531bc0
Nick DeJesus <ndejesus1227@gmail.com>
2025-11-01 20:32:06 -0400

    chore: update documentation and configuration files
    - Added new configuration files for Vite and TypeScript.
    - Updated README and other documentation files to reflect recent changes.
    - Cleaned up .gitignore files and removed unnecessary entries.
    - Introduced new content and examples in the documentation for better user guidance.

---
+++ docs/src/routes/docs/$.tsx
@@ -0,0 +1,107 @@
+import { createFileRoute, notFound } from '@tanstack/react-router'
+import { DocsLayout } from 'fumadocs-ui/layouts/docs'
+import { createServerFn } from '@tanstack/react-start'
+import { source } from '@/lib/source'
+import type * as PageTree from 'fumadocs-core/page-tree'
+import { useMemo } from 'react'
+import { docs } from '@/.source'
+import {
+  DocsBody,
+  DocsDescription,
+  DocsPage,
+  DocsTitle
+} from 'fumadocs-ui/page'
+import defaultMdxComponents from 'fumadocs-ui/mdx'
+import { createClientLoader } from 'fumadocs-mdx/runtime/vite'
+import { baseOptions } from '@/lib/layout.shared'
+
+export const Route = createFileRoute('/docs/$')({
+  component: Page,
+  loader: async ({ params }) => {
+    const slugs = params._splat?.split('/') ?? []
+    const data = await loader({ data: slugs })
+    await clientLoader.preload(data.path)
+    return data
+  }
+})
+
+const loader = createServerFn({
+  method: 'GET'
+})
+  .inputValidator((slugs: string[]) => slugs)
+  .handler(async ({ data: slugs }) => {
+    const page = source.getPage(slugs)
+    if (!page) throw notFound()
+
+    return {
+      tree: source.pageTree as object,
+      path: page.path
+    }
+  })
+
+const clientLoader = createClientLoader(docs.doc, {
+  id: 'docs',
+  component({ toc, frontmatter, default: MDX }) {
+    return (
+      <DocsPage toc={toc}>
+        <DocsTitle>{frontmatter.title}</DocsTitle>
+        <DocsDescription>{frontmatter.description}</DocsDescription>
+        <DocsBody>
+          <MDX
+            components={{
+              ...defaultMdxComponents
+            }}
+          />
+        </DocsBody>
+      </DocsPage>
+    )
+  }
+})
+
+function Page() {
+  const data = Route.useLoaderData()
+  const Content = clientLoader.getComponent(data.path)
+  const tree = useMemo(
+    () => transformPageTree(data.tree as PageTree.Folder),
+    [data.tree]
+  )
+
+  return (
+    <DocsLayout {...baseOptions()} tree={tree}>
+      <Content />
+    </DocsLayout>
+  )
+}
+
+function transformPageTree(root: PageTree.Root): PageTree.Root {
+  function mapNode<T extends PageTree.Node>(item: T): T {
+    if (typeof item.icon === 'string') {
+      item = {
+        ...item,
+        icon: (
+          <span
+            dangerouslySetInnerHTML={{
+              __html: item.icon
+            }}
+          />
+        )
+      }
+    }
+
+    if (item.type === 'folder') {
+      return {
+        ...item,
+        index: item.index ? mapNode(item.index) : undefined,
+        children: item.children.map(mapNode)
+      }
+    }
+
+    return item
+  }
+
+  return {
+    ...root,
+    children: root.children.map(mapNode),
+    fallback: root.fallback ? transformPageTree(root.fallback) : undefined
+  }
+}
