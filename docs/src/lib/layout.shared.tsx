fd3811fde1eb60cc3e5a883d240719823462cc8b
Nick DeJesus <ndejesus1227@gmail.com>
2025-11-09 09:48:24 -0500

    removed basic set up example, fixed order of things

--- docs/src/lib/layout.shared.tsx
+++ docs/src/lib/layout.shared.tsx
@@ -1,9 +1,20 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
+import * as React from 'react'
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'use-shopping-cart'
+      title: 'use-shopping-cart',
+      children: (
+        <a
+          href="https://discord.gg/TNQfW4W"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-sm font-medium hover:underline"
+        >
+          Discord
+        </a>
+      )
     }
   }
 }
